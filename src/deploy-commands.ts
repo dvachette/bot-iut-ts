db52c009b20a0cd4c69a4224a96d70c722d3a3df
dvachette <donatien.vachette@hotmail.com>
2025-02-10 17:06:50 +0100

    unregistering choose command (@old)

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,6 +1,6 @@
 
 
-import { REST, Routes } from "discord.js";
+import { REST, Routes, Snowflake } from "discord.js";
 import { config } from "./config";
 import { commands } from "./commands";
 
@@ -13,6 +13,40 @@ type DeployCommandsProps = {
 };
 
 export async function deployCommands({ guildId }: DeployCommandsProps) {
+
+  // unregister all commands before registering them again to ensure a clean state
+  try {
+    console.log("Started unregistering application (/) commands.");
+
+    // Get the current commands
+
+    const currentCommands = await rest.get(
+      Routes.applicationCommands(config.DISCORD_CLIENT_ID as Snowflake)
+    );
+    console.log("Current commands", currentCommands);
+    console.log(await rest.get(Routes.applicationCommands(config.DISCORD_CLIENT_ID as Snowflake)));
+    
+    // Remove each command individually
+    if (Array.isArray(currentCommands)) {
+      await Promise.all(
+        currentCommands.map((cmd) =>
+          
+          {console.log("Deleting command", cmd.id, cmd.name);
+
+          rest.delete(
+            Routes.applicationCommand(config.DISCORD_CLIENT_ID, cmd.id)
+          )}
+        )
+      );
+    }
+
+    console.log("Successfully unregistered application (/) commands.");
+  } catch (error) {
+    console.error("Error while unregistering commands:", error);
+  }
+
+
+
   try {
     console.log("Started refreshing application (/) commands.");
 
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const client = new Client({
   intents: ["Guilds", "GuildMessages", "DirectMessages"],
 });
 
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log("Discord bot is ready! 🤖");
 });
 
