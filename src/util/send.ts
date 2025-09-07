c9ef3f52a6e0527a6c8e381cb424345194c30c01
Donatien Vachette <donatien.vachette@hotmail.com>
2025-09-07 19:07:32 +0200

    split messages over 2000 length into smaller messages to send them

--- src/util/send.ts
+++ src/util/send.ts
@@ -11,7 +11,24 @@ export function send(channelID : String, message : String) {
         logger.error(`Message is empty, not sending to channel ${channelID}.`);
         return;
     }
-    channel.send(message.toString());
+    if (message.length >= 2000) {
+        // Split the message into chunks of 2000 characters
+        const chunks = message.match(/.{1,2000}/g);
+        if (chunks) {
+            for (const chunk of chunks) {
+                channel.send(chunk);
+            }
+            logger.info(`Message sent to channel ${channelID} in ${chunks.length} parts.`);
+        } else {
+            logger.error(`Failed to split message for channel ${channelID}.`);
+        }
+        return;
+    }
+    else {
+        logger.info(`Sending message to channel ${channelID}: ${message}`);
+        channel.send(message.toString());
+    }
+    
     logger.info(`Message sent to channel ${channelID}: ${message}`);
 }
 
