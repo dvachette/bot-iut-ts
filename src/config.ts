fe0b1b59b0af955364b88435f91926e6e991bf16
Donatien Vachette <donatien.vachette@hotmail.com>
2026-09-18 13:41:58 +0200

    using easy.env for easyer .env verification

--- src/config.ts
+++ src/config.ts
@@ -1,60 +1,18 @@
 import dotenv from "dotenv";
 import { logger } from "./logger";
+import { required } from "@dvachette/easy.env";
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID, CONF_YAML_PATH, GUILD_ID, GROUPS_FILE, PERMISSIONS_FILE, ADMIN_ROLE_ID, NO_CLASS_ROLE_ID} = process.env;
-
-if (!DISCORD_TOKEN) {
-  logger.error("DISCORD_TOKEN is not set in the environment variables");
-  throw new Error("DISCORD_TOKEN is not set in the environment variables");
-}
-if (!DISCORD_CLIENT_ID) {
-  logger.error("DISCORD_CLIENT_ID is not set in the environment variables");
-  throw new Error("DISCORD_CLIENT_ID is not set in the environment variables");
-}
-if (!CONF_YAML_PATH) {
-  logger.error("CONF_YAML_PATH is not set in the environment variables");
-  throw new Error("CONF_YAML_PATH is not set in the environment variables");  
-}
-if (!GUILD_ID) {
-  logger.error("GUILD_ID is not set in the environment variables");
-  throw new Error("GUILD_ID is not set in the environment variables");
-}
-if (!GROUPS_FILE) {
-  logger.error("GROUPS_FILE is not set in the environment variables");
-  throw new Error("GROUPS_FILE is not set in the environment variables");
-}
-if (!PERMISSIONS_FILE) {
-  logger.error("PERMISSIONS_FILE is not set in the environment variables");
-  throw new Error("PERMISSIONS_FILE is not set in the environment variables");
-}
-if (!ADMIN_ROLE_ID) {
-  logger.error("ADMIN_ROLE_ID is not set in the environment variables");
-  throw new Error("ADMIN_ROLE_ID is not set in the environment variables");
-}
-if (!NO_CLASS_ROLE_ID) {
-  logger.error("NO_CLASS_ROLE_ID is not set in the environment variables");
-  throw new Error("NO_CLASS_ROLE_ID is not set in the environment variables");
-}
-
-
-if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !CONF_YAML_PATH || !GUILD_ID || !GROUPS_FILE || !PERMISSIONS_FILE || !ADMIN_ROLE_ID || !NO_CLASS_ROLE_ID) {
-  logger.error("Missing environment variables");
-  throw new Error("Missing environment variables");
-}
-
-
-
 export const config = {
-  DISCORD_TOKEN,
-  DISCORD_CLIENT_ID,
-  CONF_YAML_PATH,
-  GUILD_ID,
-  GROUPS_FILE,
-  PERMISSIONS_FILE,
-  ADMIN_ROLE_ID,
-  NO_CLASS_ROLE_ID
+  DISCORD_TOKEN: required("DISCORD_TOKEN"),
+  DISCORD_CLIENT_ID: required("DISCORD_CLIENT_ID"),
+  CONF_YAML_PATH: required("CONF_YAML_PATH"),
+  GUILD_ID: required("GUILD_ID"),
+  GROUPS_FILE: required("GROUPS_FILE"),
+  PERMISSIONS_FILE: required("PERMISSIONS_FILE"),
+  ADMIN_ROLE_ID: required("ADMIN_ROLE_ID"),
+  NO_CLASS_ROLE_ID: required("NO_CLASS_ROLE_ID"),
 };
 
 
